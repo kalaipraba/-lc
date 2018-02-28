2e84857fe010dc8227d02b3a2503e477c5b913cc
kalaipraba <35525844+kalaipraba@users.noreply.github.com>
2018-02-28 15:54:56 +0530

    R.C

---
+++ R.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main()
+{
+    float n;
+    clrscr();
+
+    printf("Enter the number:-");
+    scanf("%f",&n);
+
+    printf("The integer number after rounded is  %5.0f",n);
+  return0;
+}

188559fb6b528c224b0b4d846d9611c9b8bc6f49
kalaipraba <35525844+kalaipraba@users.noreply.github.com>
2018-02-28 16:10:41 +0530

    SQUARE.C

---
+++ SQUARE.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+    #include<conio.h>
+    int main()
+    {
+        int n,sqr=0;
+        clrscr();
+        printf("\n\t ENTER THE NO.:= ");
+        scanf("%d",&n);
+        sqr=n*n;
+        printf("\n\t SQUARE OF %d is %d .",n,sqr);
+   return 0;
+    }
